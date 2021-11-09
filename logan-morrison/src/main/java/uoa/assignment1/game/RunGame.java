cc58468437ca292a1b78b60119228b32f12c760b
LOGAN MORRISON <l.morrison1.21@abdn.ac.uk>
2021-11-09 19:09:56 +0000

    Task 3.2

--- logan-morrison/src/main/java/uoa/assignment1/game/RunGame.java
+++ logan-morrison/src/main/java/uoa/assignment1/game/RunGame.java
@@ -1,5 +1,6 @@
 package uoa.assignment1.game;
 
+import java.util.Scanner;
 public class RunGame {
 
 	private static boolean gameOver = false;
@@ -8,10 +9,16 @@ public class RunGame {
 		
         int height = Integer.parseInt(args[0]);
         int width = Integer.parseInt(args[1]);
+        int round = 0;
         
         Game game = new Game(height, width);
         
         while (!gameOver) {
+            round++;
+            String roundStr = Integer.toString(round);
+            System.out.println("Round " + roundStr);
+            Scanner reader = new Scanner(System.in);
+            
 			//complete the code
 		}
 	}
